252adb826368ace3028879800acdd986428be156
Justin <justinshajuk@gmail.com>
2022-10-14 14:53:55 +0530

    Third commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,4 @@ from django.http import HttpResponse
 # Create your views here,
 
 def index(request):
-    d = {
-        "name": "Arun",
-        "age" : 30
-    }
-    
-    return HttpResponse("<b>Hello Worlds</b>")
+    return HttpResponse("assignment.html")
