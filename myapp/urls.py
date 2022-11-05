8f813b76950e0cbb270289dc9d83761740a22d45
Justin <justinshajuk@gmail.com>
2022-11-06 02:40:22 +0530

    12th commit 25th

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path,include
 from myapp.views import index
-from .views import news_one, product_details,products,add_product
+from .views import news_one, product_details,products,add_product,update_product,delete_product
 
 urlpatterns = [
     path('',index),
     path('new/',news_one),
     path('products/',products,name='products'),
     path('products/<int:id>',product_details,name='product_details'),
-    path('products/add',add_product,name='add_product')
+    path('products/add',add_product,name='add_product'),
+    path('products/update/<int:id>',update_product,name='update_product'),
+    path('products/delete/<int:id>',delete_product,name='delete_product')
 ]
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -66,3 +66,37 @@ def add_product(request):
         
            
     return render(request,'myapp/add_product.html')
+
+def update_product(request,id):
+    p = product.objects.get(id=id)
+    context = {'p':p}
+    
+    if request.method == 'POST':
+        p.name= request.POST.get('name')
+        p.price = request.POST.get('price')
+        p.description = request.POST.get('desc')
+        
+        try:
+            p.image = request.FILES['upload']
+        except :
+            pass
+            
+            
+        # p = product(name=name,price=price,description=desc,image=image)
+        p.save()
+        
+        return redirect('/myapp/products')
+        
+           
+    return render(request,'myapp/update_product.html',context=context)
+
+def delete_product(request,id):
+    p = product.objects.get(id=id)
+    context = {'p':p}
+    
+    if request.method == 'POST':
+        p.delete()
+        return redirect('/myapp/products')
+        
+           
+    return render(request,'myapp/delete_product.html',context=context)
